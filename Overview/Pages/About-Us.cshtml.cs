3980c23e9bcc6cb2690aeeb14f88947e9be0b489
Kevin Siraki <kevsiraki@gmail.com>
2024-11-12 20:18:45 -0800

    Automated commit for Docker image update

---
+++ Overview/Pages/About-Us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Overview.Pages
+{
+    public class About_UsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
