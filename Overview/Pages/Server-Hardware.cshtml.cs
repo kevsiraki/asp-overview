47d949e0a4dfb65465b4f1c246a9526a0f73a832
Kevin Siraki <kevsiraki@gmail.com>
2024-11-22 23:35:03 -0800

    Automated commit for Docker image update.

---
+++ Overview/Pages/Server-Hardware.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Overview.Pages
+{
+    public class Server_HardwareModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
