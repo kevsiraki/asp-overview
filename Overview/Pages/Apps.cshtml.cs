fbd5e076a342fceb4bd6ac7481c7e02132bc3ce8
Kevin Siraki <kevsiraki@gmail.com>
2025-06-22 18:22:11 -0700

    Automated commit for Docker image update.

---
+++ Overview/Pages/Apps.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Overview.Pages
+{
+    public class AppsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Overview/Pages/Systems.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Overview.Pages
+{
+    public class SystemsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
